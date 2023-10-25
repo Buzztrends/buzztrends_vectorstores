c27c256d1e4c7cc9a89917169a29d87f08538ff8
zee-1 <zaid.khan21800@gmail.com>
2023-10-25 23:50:16 +0530

    Feature: writer interface

---
+++ writer/writer.py
@@ -0,0 +1,12 @@
+class Writer:
+    def __init__(self) -> None:
+        pass
+
+    def create(collection_name:str):
+        pass
+
+    def update(collection_name:str):
+        pass
+
+    def update(collection_name:str):
+        pass

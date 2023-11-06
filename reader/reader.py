81f9e853a75589752ce6c51dce15bfa524a64749
maxDeCoder <vedantjumle@gmail.com>
2023-11-07 00:01:47 +0530

    update connectors

--- reader/reader.py
+++ reader/reader.py
@@ -13,8 +13,8 @@ class Reader:
         else:
             self.retriever = Chroma(client=__client,collection_name=collection,embedding_function=OpenAIEmbeddings(openai_api_key=openai_api_key,model="text-embedding-ada-002"))
 
-    def search(self,query,n=20):
-        return self.retriever.search(query=query,search_type="similarity")
+    def search(self,query,n=20,filter=None):
+        return self.retriever.similarity_search(query=query, search_type="similarity", k=n, filter=filter)
     
 if __name__ == "__main__":
     r = Reader(host="localhost",port=8000,collection="my_test",openai_api_key=os.environ["OPENAI_API_KEY"])
